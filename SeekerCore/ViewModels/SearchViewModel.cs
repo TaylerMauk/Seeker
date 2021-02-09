8a313e3379abcf473e681c0f743fcc97759a77c0
Tayler Mauk <44530136+TaylerMauk@users.noreply.github.com>
2021-02-08 20:41:37 -0800

    Add structure for file info display

--- SeekerCore/ViewModels/SearchViewModel.cs
+++ SeekerCore/ViewModels/SearchViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
 using System.Windows;
 using System.Windows.Input;
 using SeekerCore.Model;
@@ -156,6 +157,100 @@ namespace SeekerCore.ViewModels
 
         public ObservableCollection<string> SearchResultEntries { get; private set; }
 
+        /// <summary>
+        /// Index of selected file from search results
+        /// </summary>
+        public int SelectedFileIndex
+        {
+            get
+            {
+                return m_selectedFileIndex;
+            }
+            set
+            {
+                m_selectedFileIndex = value;
+
+                OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(SelectedFileIndex)));
+                OnSelectedResultFileIndexChanged();
+            }
+        }
+        private int m_selectedFileIndex;
+
+        public string SelectedFilePath
+        {
+            get
+            {
+                return m_selectedFilePath;
+            }
+            set
+            {
+                m_selectedFilePath = value;
+
+                OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(SelectedFilePath)));
+            }
+        }
+        private string m_selectedFilePath;
+
+        public string SelectedFileName
+        {
+            get
+            {
+                return m_selectedFileName;
+            }
+            set
+            {
+                m_selectedFileName = value;
+
+                OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(SelectedFileName)));
+            }
+        }
+        private string m_selectedFileName;
+
+        public string SelectedFileLastAccessTime
+        {
+            get
+            {
+                return m_selectedFileLastAccesTime;
+            }
+            set
+            {
+                m_selectedFileLastAccesTime = value;
+
+                OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(SelectedFileLastAccessTime)));
+            }
+        }
+        private string m_selectedFileLastAccesTime;
+
+        public string SelectedFileLastWriteTime
+        {
+            get
+            {
+                return m_selectedFileLastWriteTime;
+            }
+            set
+            {
+                m_selectedFileLastWriteTime = value;
+
+                OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(SelectedFileLastWriteTime)));
+            }
+        }
+        private string m_selectedFileLastWriteTime;
+
+        public string SelectedFileCreationTime
+        {
+            get
+            {
+                return m_selectedFileCreationTime;
+            }
+            set
+            {
+                m_selectedFileCreationTime = value;
+
+                OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(SelectedFileCreationTime)));
+            }
+        }
+        private string m_selectedFileCreationTime;
+
         public ICommand ExecuteSearchCommand { get; set; }
 
         public ICommand AddSearchDirectoryCommand { get; set; }
@@ -180,6 +275,7 @@ namespace SeekerCore.ViewModels
             m_languageParser = new LanguageParser();
             m_searchAgent = new SearchAgent();
             m_searchDirectoryRemovalIndex = -1;
+            m_selectedFileIndex = -1;
 
             m_searchAgent.StateChanged += OnSearchAgentStateChanged;
         }
@@ -213,7 +309,7 @@ namespace SeekerCore.ViewModels
                     if (null != m_searchAgent.Results.entries)
                     {
                         foreach (string entry in m_searchAgent.Results.entries)
-                            SearchResultEntries.Add(entry);
+                            SearchResultEntries.Add(entry.Substring(entry.LastIndexOf("\\") + 1));
                         SearchTotalResultCount = m_searchAgent.Results.count;
                     }
                 });
@@ -221,7 +317,22 @@ namespace SeekerCore.ViewModels
 
             App.Current.Dispatcher.Invoke(() => {
                 ((RelayCommand)ExecuteSearchCommand).RaiseCanExecuteChanged();
+                ((RelayCommand)AddSearchDirectoryCommand).RaiseCanExecuteChanged();
+                ((RelayCommand)RemoveSearchDirectoryCommand).RaiseCanExecuteChanged();
             });
+        }
+
+        private void OnSelectedResultFileIndexChanged()
+        {
+            if (m_selectedFileIndex < 0 || m_selectedFileIndex > m_searchAgent.Results.count)
+                return;
+
+            FileInfo fileInfo = new FileInfo(m_searchAgent.Results.entries[m_selectedFileIndex]);
+            SelectedFilePath = fileInfo.FullName;
+            SelectedFileName = fileInfo.Name;
+            SelectedFileCreationTime = fileInfo.CreationTime.ToString("F");
+            SelectedFileLastAccessTime = fileInfo.LastAccessTime.ToString("F");
+            SelectedFileLastWriteTime = fileInfo.LastWriteTime.ToString("F");
         }
 
         private void ExecuteSearch()
@@ -258,7 +369,10 @@ namespace SeekerCore.ViewModels
 
         private bool CanExecuteAddSearchDirectory()
         {
-            return null != NewSearchDirectory && NewSearchDirectory != string.Empty && System.IO.Directory.Exists(NewSearchDirectory);
+            return m_searchAgent.State == SearchAgent.ActivityState.INACTIVE &&
+                null != NewSearchDirectory &&
+                NewSearchDirectory != string.Empty &&
+                Directory.Exists(NewSearchDirectory);
         }
 
         private void RemoveSearchDirectory()
@@ -269,7 +383,9 @@ namespace SeekerCore.ViewModels
 
         private bool CanExecuteRemoveSearchDirectory()
         {
-            return SearchDirectories.Count > 0 && SearchDirectoryRemovalIndex != -1;
+            return m_searchAgent.State == SearchAgent.ActivityState.INACTIVE &&
+                SearchDirectories.Count > 0 &&
+                SearchDirectoryRemovalIndex != -1;
         }
     }
 }
